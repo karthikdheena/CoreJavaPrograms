d094caf84bee6445871d452f70444e851786471f
karthikdheena <karthikeyan2662005@gmail.com>
2026-03-03 13:43:05 +0530

    FinalKey.java

---
+++ Core/FinalKey.java
@@ -0,0 +1,42 @@
+package Core;
+
+final class Hello{	//	final class
+	
+	final int num = 10; // final variable
+	
+	
+	final public void show() { 	//final method
+		
+	System.out.println("hello karthik");	
+	}
+	
+	public void add(int r1 , int r2) {
+		
+	System.out.println(r1 + r2);
+		
+	}
+}
+
+//class Bye extends Hello {
+
+//public void show()
+	
+//}
+
+public class FinalKey {
+
+	public static void main(String[] args) {
+		
+		Hello obj = new Hello();
+		
+		obj.show();
+		obj.add(20, 40);
+		
+		
+		
+		
+		
+
+	}
+
+}
